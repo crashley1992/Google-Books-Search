8cbad22093b1f35e6822a5c17cbbce3b43170c99
Ashley Clarke <crashley92@gmail.com>
2019-12-21 12:21:26 -0800

    another attempt

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-app.get('*',(req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
-  });
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 // Connect to the Mongo DB
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/GoogleBooks";
