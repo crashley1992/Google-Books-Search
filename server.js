10700152b541aabdf703abbfdfb1882967868562
Ashley Clarke <crashley92@gmail.com>
2019-12-21 12:28:15 -0800

    another attempt

--- server.js
+++ server.js
@@ -18,10 +18,10 @@ app.use(express.json());
 
 // Static assets
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(__dirname, "client/build"));
 }
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/../client/build/index.html'));
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 // Connect to the Mongo DB
